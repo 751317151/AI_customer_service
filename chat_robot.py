fa61aad36c9007cd7bd9d904180a023f603c183a
hh <hh751317151@163.com>
2019-07-03 14:54:02 +0800

    完成了和微信服务器对话和校验token的功能

--- chat_robot.py
+++ chat_robot.py
@@ -4,7 +4,12 @@
 
 import urllib.request
 import urllib.parse
+import flask
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
 
+
+app = flask.Flask(__name__)
 
 def get_robot_reply(question):
     '''
@@ -34,12 +39,34 @@ def get_robot_reply(question):
 
     return answer
 
+@app.route("/wx",methods=["GET","POST"])
+def weixin_handler():
+    token = "huahao"
+    signature = flask.request.args.get("signature")
+    timestamp = flask.request.args.get("timestamp")
+    nonce = flask.request.args.get("nonce")
+    echostr = flask.request.args.get("echostr")
+
+    try:
+        # 校验token
+        check_signature(token, signature, timestamp, nonce)
+    except InvalidSignatureException:
+        # 处理异常情况或忽略
+        flask.abort(403)  # 校验token失败，证明这条消息不是微信服务器发送过来的
+    
+    if flask.request.method == "GET":
+        return echostr
+    elif flask.request.method == "POST":
+        print(flask.request.data)
+
+
 if __name__ == '__main__':
-    # 测试get_robot_reply函数
-    while True:
-        question = input("(输入(退出)即可退出聊天)我说： ")       
-        if question == "退出":
-            break
-        answer = get_robot_reply(question)
-        print("小魔仙说：%s"%answer)
+    # # 测试get_robot_reply函数
+    # while True:
+    #     question = input("(输入(退出)即可退出聊天)我说： ")       
+    #     if question == "退出":
+    #         break
+    #     answer = get_robot_reply(question)
+    #     print("小魔仙说：%s"%answer)
+    app.run(debug=True,host="0.0.0.0",port="80")
         
